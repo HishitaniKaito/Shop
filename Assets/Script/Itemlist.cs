886c92393cd2ef0aa5bb3d1d8d3d0e4bb58979b3
KAITO <90747002+HishitaniKaito@users.noreply.github.com>
2023-07-10 12:53:54 +0900

    no message

---
+++ Assets/Script/Itemlist.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Itemlist : MonoBehaviour
+{
+    Gamemanager _gm;
+    [SerializeField] GameObject _itembutton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _gm = FindObjectOfType<Gamemanager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void List()
+    {
+        for (int i = 0; i > _gm._item.Length - 1; i++)
+        {
+            if (_gm._item[i] > 0)
+            {
+                var x = Instantiate(_itembutton);
+                switch (i)
+                {
+                    case 0:
+
+                        break;
+                    case 1:
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                    case 4:
+                        break;
+                }
+            }
+        }
+    }
+}
